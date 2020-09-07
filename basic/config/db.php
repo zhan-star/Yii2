56f02f2a19cf16ce6ce9d38c25d58095c8b47045
zhan-star <janik150802@gmail.com>
2020-09-07 17:00:02 +0600

    Fourth work

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,11 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
